803a2538d810ef6adb520b9dffa66d055e70f26a
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-11-03 14:32:12 +0530

    Initial Commit

---
+++ Meal App/Meal_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_AppApp.swift
+//  Meal App
+//
+//  Created by Isuru Ariyarathna on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct Meal_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meal AppTests/Meal_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_AppTests.swift
+//  Meal AppTests
+//
+//  Created by Isuru Ariyarathna on 2024-11-03.
+//
+
+import Testing
+@testable import Meal_App
+
+struct Meal_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
